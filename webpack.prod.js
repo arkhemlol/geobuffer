d7b87083e6429f18f7e86e7cc8a35cfa45391ccf
arkhemlol <arkhemlol@gmail.com>
2020-03-24 16:41:16 +0300

    added travis deploy config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,15 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
+  devtool: 'source-map',
   output: {
-    filename: 'geobuffer.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'umd',
     library: 'geobuffer'
-  }
+  },
+  plugins: [new CleanWebpackPlugin()]
 };
