eaef16274774d0d17794a69d64372e3dd24a3a60
arkhemlol <>
2021-02-03 20:16:54 +0300

    update packages

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,45 +1,45 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
-  entry: './src/examples/leaflet-draw-example.js',
+  mode: "development",
+  devtool: "source-map",
+  entry: "./src/examples/leaflet-draw-example.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle[name].js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
-      }
-    ]
+        use: ["file-loader"],
+      },
+    ],
   },
   resolve: {
     alias: {
-      buffer: path.resolve(__dirname, 'src/index.js')
-    }
+      buffer: path.resolve(__dirname, "src/index.js"),
+    },
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/examples/index.html'),
-      path: path.resolve(__dirname, 'dist')
-    })
+      template: path.resolve(__dirname, "src/examples/index.html"),
+      path: path.resolve(__dirname, "dist"),
+    }),
   ],
   optimization: {
     usedExports: true,
-    runtimeChunk: true
+    runtimeChunk: true,
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    hot: true
-  }
+    contentBase: path.join(__dirname, "dist"),
+    hot: true,
+  },
 };
--- webpack.gh-pages.js
+++ webpack.gh-pages.js
@@ -1,40 +1,40 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
-  entry: './src/examples/leaflet-draw-example.js',
+  mode: "production",
+  devtool: "source-map",
+  entry: "./src/examples/leaflet-draw-example.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    filename: "bundle[name].js",
+    path: path.resolve(__dirname, "public"),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
-      }
-    ]
+        use: ["file-loader"],
+      },
+    ],
   },
   resolve: {
     alias: {
-      buffer: path.resolve(__dirname, 'src/index.js')
-    }
+      buffer: path.resolve(__dirname, "src/index.js"),
+    },
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'src/examples/index.html'),
-      path: path.resolve(__dirname, 'public')
-    })
+      template: path.resolve(__dirname, "src/examples/index.html"),
+      path: path.resolve(__dirname, "public"),
+    }),
   ],
   optimization: {
-    usedExports: true
-  }
+    usedExports: true,
+  },
 };
